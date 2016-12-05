cd25980f892f3e9701769c232bb710a83c95c30f
William Hinschink <william@sft.inf.br>
2016-12-05 09:18:13 -0200

    Tratando Erros

--- demo-project/demo-service-app/src/main/java/br/com/example/init/Application.java
+++ demo-project/demo-service-app/src/main/java/br/com/example/init/Application.java
@@ -1,12 +1,16 @@
 package br.com.example.init;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "br.com.example")
 public class Application {
-
+	private static Logger logger = LogManager.getLogger(Application.class);
 	public static void main(String[] args) {
+		logger.info("Logger Aqui");
 		SpringApplication.run(Application.class, args);
+		logger.info("Logger fim aqui");
 	}
 }
